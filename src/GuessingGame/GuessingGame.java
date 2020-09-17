8359e902510f862ffcdb27537850d50d903fa17d
Sarah Temple <sarahelisetemple@gmail.com>
2020-09-17 09:36:57 -0700

    Final edits to make a completed game

--- src/GuessingGame/GuessingGame.java
+++ src/GuessingGame/GuessingGame.java
@@ -13,11 +13,11 @@ public class GuessingGame {
 		boolean stillPlaying = true;
 		
 		while (stillPlaying== true) {
-			
+			System.out.println("I am thinking of a number between 0 and 50. Type your best guess below and I will tell you if it is too high or low.");
 			int randomNum = (int) (Math.random() * 51);
-			System.out.println(randomNum);
 			Scanner scanner = new Scanner(System.in);
 			int guess = -1;
+			int numtry = 0;
 			while (guess != randomNum) {
 				String input = scanner.nextLine();
 				System.out.println("You typed: " + input);
@@ -25,10 +25,27 @@ public class GuessingGame {
 				System.out.println(guess);
 				if (guess < randomNum) {
 					System.out.println("Your guess is less than the random number.");
-				
+					System.out.println("Try again!");
+					numtry = numtry + 1;
+				}
+				if (guess > randomNum) {
+					System.out.println("Your guess is more than the random number.");
+					System.out.println("Try again!");
+					numtry = numtry + 1;
 				}
 			}
+			if (guess == randomNum){
+				System.out.println("You are correct!");
+				System.out.println("You guessed incorrectly " + numtry + " times.");
+			}
 			System.out.println("Play again?");
+			String yesno = scanner.nextLine();
+			if (yesno.equals("yes") || yesno.equals("y") ) {
+				stillPlaying = true;
+			}
+			else {
+				stillPlaying = false;
+			}
 		}
 	}
 	
